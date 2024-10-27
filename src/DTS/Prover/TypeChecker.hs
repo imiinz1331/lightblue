53c64c7f73ff38d0f2f9ba504bb39d96a1f7c39a
imiinz1331 <imiinz1331@gmail.com>
2024-10-28 02:27:24 +0900

    fix: fix isPred Filter

--- src/DTS/Prover/TypeChecker.hs
+++ src/DTS/Prover/TypeChecker.hs
@@ -565,10 +565,9 @@ getJudgements env ((tm, ty):rest) =
       isEntity _ = False
       isPred (UJudgement _ _ term) = 
           case term of
-            UD.App (UD.App f x) y ->
+            UD.App f x ->
                 not (containsFunctionType f) && 
-                not (containsFunctionType x) && 
-                not (containsFunctionType y)
+                not (containsFunctionType x)
             _ -> False
 
 containsFunctionType :: UD.Preterm -> Bool
@@ -588,6 +587,7 @@ loop env (tm, ty) = case ty of
 sigmaE :: (UD.Preterm, UD.Preterm) -> [(UD.Preterm, UD.Preterm)]
 sigmaE (m, (UD.Sigma a b)) = [((UD.Proj UD.Fst m), a), ((UD.Proj UD.Snd m), (UD.subst b (UD.Proj UD.Fst m) 0))]
 
+{-
 -- テスト関数
 testEliminateSigma :: Bool
 testEliminateSigma = and
@@ -647,6 +647,78 @@ testWalkingManSigma =
     
 runTests = do
   let allTestsPassed = testEliminateSigma
+  putStrLn $ if allTestsPassed
+    then "All tests passed!"
+    else "Some tests failed."
+    -}
+
+-- getJudgesのテスト関数
+testGetJudges :: Bool
+testGetJudges = and
+  [ testManWalking,
+    testManEnteredWhistled
+  ]
+
+testManWalking :: Bool
+testManWalking =
+    let env = []
+        -- S : (u : (x : entity × man(x))) × walk(π1(u))
+        term = UD.Con (T.pack "S") 
+        sigmaType = UD.Sigma 
+                        (UD.Sigma 
+                            (UD.Con (T.pack "entity")) 
+                            (UD.App (UD.Con (T.pack "man")) (UD.Var 0)))
+                        (UD.App (UD.Con (T.pack "walk")) (UD.Proj UD.Fst (UD.Var 0)))
+        result = getJudgements env [(term, sigmaType)]
+        expected = [
+            ([UJudgement env (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S")))) (UD.Con (T.pack "entity"))], -- π1π1S : entity
+             [UJudgement env (UD.Proj UD.Snd (UD.Proj UD.Fst (UD.Con (T.pack "S"))))  -- π2π1S : man(π1π1S)
+              (UD.App (UD.Con (T.pack "man")) (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S"))))),
+              UJudgement env (UD.Proj UD.Snd (UD.Con (T.pack "S"))) -- π2S : walk(π1π1S)
+              (UD.App (UD.Con (T.pack "walk")) (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S")))))])
+          ]
+        traceResult = trace ("Result: " ++ show result) result
+        traceExpected = trace ("Expected: " ++ show expected) expected
+        isEqual = traceResult == traceExpected
+        -- isEqual = seq (trace ("Result: " ++ show result) ()) True
+    in trace ("WalkingMan test: " ++ show isEqual) isEqual
+
+testManEnteredWhistled :: Bool
+testManEnteredWhistled =
+    let env = []
+        -- 1文目: A man entered.
+        term1 = UD.Con (T.pack "S1")
+        type1 = UD.Sigma
+                    (UD.Sigma
+                        (UD.Con (T.pack "entity"))
+                        (UD.App (UD.Con (T.pack "man")) (UD.Var 0)))
+                    (UD.App (UD.Con (T.pack "enter")) (UD.Proj UD.Fst (UD.Var 0)))
+
+        -- 2文目: He whistled.
+        term2 = UD.Con (T.pack "S2")
+        type2 = UD.App
+                    (UD.Con (T.pack "whistle"))
+                    (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S1"))))
+
+        result = getJudgements env [(term1, type1), (term2, type2)]
+
+        expected = [
+            -- 1文目の結果
+            ([UJudgement env (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S1")))) (UD.Con (T.pack "entity"))],
+             [UJudgement env (UD.Proj UD.Snd (UD.Proj UD.Fst (UD.Con (T.pack "S1")))) (UD.App (UD.Con (T.pack "man")) (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S1"))))),
+              UJudgement env (UD.Proj UD.Snd (UD.Con (T.pack "S1"))) (UD.App (UD.Con (T.pack "enter")) (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S1")))))]),
+            -- 2文目の結果
+            ([],
+             [UJudgement env (UD.Con (T.pack "S2")) (UD.App (UD.Con (T.pack "whistle")) (UD.Proj UD.Fst (UD.Proj UD.Fst (UD.Con (T.pack "S1")))))])
+          ]
+
+        traceResult = trace ("Result: " ++ show result) result
+        traceExpected = trace ("Expected: " ++ show expected) expected
+        isEqual = traceResult == traceExpected
+    in trace ("Man Entered Whistled test: " ++ show isEqual) isEqual
+
+runTests = do
+  let allTestsPassed = testGetJudges
   putStrLn $ if allTestsPassed
     then "All tests passed!"
     else "Some tests failed."
